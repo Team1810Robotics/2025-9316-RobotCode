5f3325c4dbb90c5d7c9b41940d473e44cdeaf089
Cenzell <ethanhartshorn488@outlook.com>
2025-01-30 20:57:39 -0600

    started the constants file

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,22 @@
+package frc.robot;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+
+public class Constants {
+    public static final class VisionConstants{
+       
+        public static final double CAMERA_HIGHT = 0.0;
+        public static final double APRILTAG_RED_SHOOTER_HEIGHT = 0.0;
+        public static final double CAMERA_PITCH = 0.0;
+        
+        public static final AprilTagFieldLayout APRIL_TAG_FIELD_LAYOUT =
+            AprilTagFields.k2025Reefscape.loadAprilTagLayoutField(); 
+        
+    }
+    
+        
+}
+
+
+
--- src/main/java/frc/robot/subsystems/Vision_Subsystem.java
+++ src/main/java/frc/robot/subsystems/Vision_Subsystem.java
@@ -1,6 +1,41 @@
 package frc.robot.subsystems;
 
-public class Vision_Subsystem {
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Constants.VisionConstants;
+import java.util.List;
+import java.util.Optional;
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.PhotonUtils;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+public class Vision_Subsystem extends SubsystemBase {
     
+AprilTagFieldLayout aprilTagFieldLayout = 
+Constants.APRIL_TAG_FIELD_LAYOUT;
+
+    PhotonCamera camera;
+    PhotonPoseEstimator photonPoseEstimator;
+    PhotonPipelineResult result;  
+
+     public Vision_Subsystem() {
+        camera = new PhotonCamera(VisionConstants.TARGET_CAMERA);
+        photonPoseEstimator =
+                new PhotonPoseEstimator(
+                        AprilTagFieldLayout,
+                        PoseStrategy.CLOSEST_TO_REFERENCE_POSE,
+                        camera,
+                        robotToCam);
+        result = camera.getLatestResult();
+    }
 }
     
