7dedd8d5140bdeccbc36d29fc522075f97c96bf2
LukeM-CTEC-232 <megeeluk000@usd232.org>
2025-02-01 10:12:40 -0600

    Starting Point, don't know if this will actually work or not

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,14 @@
+package frc.robot;
+
+
+public class Constants {
+    public static final class ElevatorConstants{
+
+    private static final double ELEVATOR_UP_SPEED = 0.5;
+    private static final double ELEVATOR_DOWN_SPEED = -0.5;
+
+    private static final double ELEVATOR_UP_POSITION = 0.25;
+    private static final double ELEVATOR_DOWN_POSITION = 0;
+
+    }
+}
--- src/main/java/frc/robot/subsystems/Elevator_Subsystem.java
+++ src/main/java/frc/robot/subsystems/Elevator_Subsystem.java
@@ -1,5 +1,32 @@
 package frc.robot.subsystems;
 
-public class Elevator_Subsystem {
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+public class Elevator_Subsystem extends SubsystemBase {
+    private WPI_TalonSRX elevatorMotor;
+    private Encoder elevatorEncoder;
+    private PIDController elevatorPID;
     
+
+    public void ElevatorSubsystem() {
+        elevatorMotor = new WPI_TalonSRX(5);
+        elevatorEncoder = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
+        elevatorPID = new PIDController(1.0, 0.0, 0.0);
+        elevatorPID.setSetpoint(0);  // Starting setpoint
+        elevatorPID.setTolerance(1.0);  // Set tolerance for how close we need to get to the target (in encoder counts)
+    }
+
+    public void moveElevator(double speed) {
+        elevatorMotor.set(speed);
+    }
+
+    public void moveToPosition(double setpoint) {
+        elevatorPID.setSetpoint(setpoint);
+        double output = elevatorPID.calculate(elevatorEncoder.getDistance());
+        elevatorMotor.set(output);
+    }
 }
+
