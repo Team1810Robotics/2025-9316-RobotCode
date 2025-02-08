3d2ab8af5e4d942e3f40c2dbdb3ddd8ce55dd8c5
Wyatt <luxwya000@usd232.org>
2025-02-08 09:24:23 -0600

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,14 +1,14 @@
 package frc.robot;
 
-
 public class Constants {
     public static final class ElevatorConstants{
 
     private static final double ELEVATOR_UP_SPEED = 0.5;
     private static final double ELEVATOR_DOWN_SPEED = -0.5;
 
-    private static final double ELEVATOR_UP_POSITION = 0.25;
-    private static final double ELEVATOR_DOWN_POSITION = 0;
-
+    public static final double ELEVATOR_UP_POSITION = 0.25;
+    public static final double ELEVATOR_DOWN_POSITION = 0;
+    public static final int elevatorMotor1 = 10;
+    public static final int elevatorMotor2 = 9;
     }
 }
