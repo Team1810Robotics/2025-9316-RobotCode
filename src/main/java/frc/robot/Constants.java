a1cfe971ca6c78412d5ea2de5eba36a17442f1c5
thomazac000@usd232.org <thomazac000@usd232.org>
2025-02-21 20:12:28 -0600

    improved documentation on elevator constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,17 +14,18 @@ public class Constants {
     public static final class ElevatorConstants {
         public static final double ELEVATOR_UP_POSITION = 0.25; // Units? (e.g., percentage, encoder ticks)
         public static final double ELEVATOR_DOWN_POSITION = 0; // Units?
-        public static final int ELEVATOR_MOTOR_1_ID = 10; // Motor ID
-        public static final int ELEVATOR_MOTOR_2_ID = 9;  // Motor ID
-        public static final int LIMIT_SWITCH_PORT = 0; // Digital Input Port
+        public static final int ELEVATOR_MOTOR_1_ID = 9; // Motor ID right
+        public static final int ELEVATOR_MOTOR_2_ID = 10;  // Motor ID left
+        // public static final int LIMIT_SWITCH_PORT = 0; // Digital Input Port
         public static final int ELEVATOR_ENCODER_PORT = 49; // Encoder port
         public static final int smartCurrentLimit = 40;
         public static final class ElevatorLeft {
-            public static final int kFollowerModeLeaderId = 9;
+            public static final int kCanID = 10; // left
+            public static final int kFollowerModeLeaderId = 9; // left follows right
             public static final boolean kFollowerModeIsInverted = true;
         }
         public static final class ElevatorRight {
-            public static final int kCanID = 9;
+            public static final int kCanID = 9; // right
             public static final int kInputMode = 1;
             public static final int kIdleMode = 1;
             public static final boolean kInverted = false;
