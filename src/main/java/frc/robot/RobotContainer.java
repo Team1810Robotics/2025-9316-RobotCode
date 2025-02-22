4b3643d88a02010f509cd24a7501124485c01539
UrbestM8 <96326235+UrbestM8@users.noreply.github.com>
2025-02-22 14:48:29 -0600

    Added Another Auto, Added Beam Break Status To Shuffle Board

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -133,6 +133,9 @@ public class RobotContainer {
         Shuffleboard.getTab("Autonomous").add(autoChooser);
     }
 
+    private void setElastic(){
+       
+    }
      
 
     public Command getAutonomousCommand() {
